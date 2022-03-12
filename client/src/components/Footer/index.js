54af1856cf8359fad4cbd9646b0807eaa1fb3b24
Erin M <erin.michon@gmail.com>
2022-03-11 21:31:27 -0500

    added Nav and Footer inital structure in components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div >&copy; 2022 | Shave the Yak</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <header>
+            Navigation
+    </header>
+  );
+};
+
+export default Nav;
