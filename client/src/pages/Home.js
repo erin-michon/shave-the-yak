a397fa7353cf39392eb0a5d06b674ecd5fbe5379
Jordan Ophoff <noreply>
2022-03-12 13:13:25 -0500

    added tailwind

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Welcome to shave the Yak!</h1>
+      <h1 className='bg-yellow-800'>Welcome to shave the Yak!</h1>
       <h2>It is time to start.</h2>
       <a href="google.com">Click here to get started.</a>
     </div>
