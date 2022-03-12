7b3164d47731ebc77181fbb63c2e4a70f17e73cd
Erin M <erin.michon@gmail.com>
2022-03-11 23:58:35 -0500

    added react link to Nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <header>
-            Navigation
+      <div >
+        <Link to="/">
+          <h1>USERNAME</h1>
+        </Link>
+
+        <nav className="text-center">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
+      </div>
     </header>
   );
 };
