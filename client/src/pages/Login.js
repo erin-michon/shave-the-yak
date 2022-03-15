8971d182fe7e76a3c757648b55b578cdd6a63240
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2022-03-14 20:19:41 -0400

    implemented and tested signin and create account functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 
-function Login() {
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Login = (props) => {
+
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values 
+    setFormState({
+      email: '',
+      password: ''
+    });
+  };
+
   return (
     <div className="flex justify-center text-white">
       <div className="p-4 border rounded border-4 bg-slate-600 bg-opacity-50 place-content-center">
@@ -9,7 +48,7 @@ function Login() {
           ← Go to Signup
         </Link>
         <h2 className="mt-4">Login Below</h2>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="mt-4">
             <label htmlFor="email">Email address:</label>
             <input
@@ -18,6 +57,8 @@ function Login() {
               name="email"
               type="email"
               id="email"
+              value={formState.email}
+              onChange={handleChange}
             />
           </div>
           <div className="mt-4">
@@ -28,12 +69,15 @@ function Login() {
               name="password"
               type="password"
               id="pwd"
+              value={formState.password}
+              onChange={handleChange}
             />
           </div>
           <div className="mt-4">
             <button type="submit" className='border-transparent rounded border-4 bg-slate-600 hover:bg-slate-800 py-1 px-2'>Submit</button>
           </div>
         </form>
+        {error && <div>Login failed</div>}
       </div>
     </div>
   );
