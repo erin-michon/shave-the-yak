af153af226e5eccd89e944f32987a39c6cba556f
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2022-03-14 20:52:59 -0400

    updated auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -37,7 +37,7 @@ class AuthService {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
 
-    window.location.assign('/');
+    window.location.assign('/dashboard');
   }
   
   // clear token from localStorage and force logout with reload
