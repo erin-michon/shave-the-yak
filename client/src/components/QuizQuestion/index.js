4d3f0f7401969ab1a0c19d4dc5cb374914363dbc
Erin M <erin.michon@gmail.com>
2022-03-16 12:57:21 -0400

    questions rendering! BOO YA

--- client/src/components/QuizQuestion/index.js
+++ client/src/components/QuizQuestion/index.js
@@ -37,28 +37,40 @@ class QuizQuestion extends Component {
         const {userAnswer, answer, score, currentIndex} = this.state
 
         console.log("next question button clicked")
-        console.log(answer)
-        console.log(currentIndex)
+        console.log(score + " is score")
 
-        //Check if correct answer and increment score
+        //Check for correct answer and increment score
         if(userAnswer === answer){
+
             this.setState({
+
                 score: score + 1
             })
+
+            console.log("correct answer selected")
+            console.log(score + " is the current score")
+          
+        } else {
+            console.log ("wrong answer selected")
+            console.log(score + " is the current score")
         }
 
         this.setState({
             currentIndex: currentIndex + 1,
             userAnswer: null
         })
-
-        console.log(currentIndex + "is the current index")
-
-
     }
 
     componentDidMount() {
         this.loadQuiz();
+    }
+
+    // Check the answer; sets the userAnswer state and enables the next button (disabled = false)
+    checkAnswer = answer => {
+        this.setState({
+            userAnswer: answer,
+            disabled:false
+        })
     }
 
     componentDidUpdate(prevProps, prevState){
@@ -68,19 +80,18 @@ class QuizQuestion extends Component {
         if(currentIndex !== prevState.currentIndex){
             this.setState(() => {
                 return {
-                    disabled: true,
+                    question: QuizData[currentIndex].question,
+                    options : QuizData[currentIndex].options,
+                    answer: QuizData[currentIndex].correctOpt          
+                }
+            })
 
+            this.setState(() => {
+                return {
+                    disabled: true,
                 }
             })
         }
-    }
-
-    // Check the answer; sets the userAnswer state and enables the next button (disabled = false)
-    checkAnswer = answer => {
-        this.setState({
-            userAnswer: answer,
-            disabled:false
-        })
     }
 
     //Determines if the quiz has ended, if so - sets the quizEnd state to true
@@ -118,12 +129,14 @@ class QuizQuestion extends Component {
             <div>
                 <h2> {question} </h2>
                 {
-                    options.map(option => 
-                        <p key = {options.id} className={`options ${userAnswer === option? "selected" : null}`} 
-                        onClick = {() => this.checkAnswer(option)}
-                        >
-                            {option}
-                        </p>
+                    options.map((option) => 
+
+                        <ul key = {option} className={`options ${userAnswer === option? "selected" : null}`} 
+                        onClick = {() => this.checkAnswer(option)}>
+                        
+                            <li>{option}</li>
+                                                
+                        </ul>
                     )
                 }
 
@@ -144,83 +157,3 @@ class QuizQuestion extends Component {
 }
 
 export default QuizQuestion
-  
-
-
-
-//   return (
-//     <div>
-//         <div>
-//             <h2>QUESTION</h2>
-//             <ul>
-//                 <li>Choice 1</li>
-//                 <li>Choice 2</li>
-//                 <li>Choice 3</li>
-//                 <li>Choice 4</li>
-//             </ul>
-//             <button>Next Question</button>
-//         </div>
-//         <div>
-//             <h2> Your Score</h2>
-//             <h3> Current Score Shown Here</h3>
-//             <button>Submit Score</button>
-//         </div>
-        
-//     </div>
-// )
-
-
-//   render() {
-//     const {
-//         question, options, currentIndex, userAnswer, quizEnd} = this.state //get the current state
-    
-    
-//     if(quizEnd) {
-//         return (
-//             <div>
-//                 <h1>Game Over. Final score is {this.state.score} points</h1>
-//                 <p>The correct Answers for the quiz are</p>
-//                 <ul>
-//                     {QuizData.map((item, index) => (
-//                         <li className='ui floating message options'
-//                             key={index}>
-//                                 {item.answer}
-//                         </li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         )
-//     }
-            
-//     return (
-//         <div>
-//             <h2>{question}</h2>
-//             <span>{`Question ${currentIndex} of ${QuizData.length -1}`}</span>
-//             {options.map(option => (  //for each option, new paragraph
-//                 <p key={option.id} 
-//                 className={`ui floating message options
-//                 ${userAnswer === option ? "selected" : null}
-//                 `}
-//                 onClick= {() => this.checkAnswer(option)}
-
-//                 >
-//                     {option}
-//                 </p>
-//             ))}
-//             {currentIndex < QuizData.length -1 &&
-//             <button 
-//             className="ui inverted button"
-//             disabled = {this.state.disabled}
-//             onClick = {this.nextQuestionHander}
-//                 >Next Question</button>
-//             }
-//                 {currentIndex === QuizData.length -1 &&
-//                 <button
-//                 className="ui inverted button"
-//                 disabled = {this.state.disabled}
-//                 onClick = {this.finishHandler}
-//                 >Finish</button>
-//                 }
-//         </div>
-//     )
-// }
